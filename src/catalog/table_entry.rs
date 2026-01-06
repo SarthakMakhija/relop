af7f8e7a9ccf6ac111ebe746b548045d3b4ec6c7
Sarthak <sarthak.makhija@gmail.com>
2026-01-06 23:29:11 +0530

    Sarthak | Adds support for inserting Row in TableEntry

--- src/catalog/table_entry.rs
+++ src/catalog/table_entry.rs
@@ -1,18 +1,75 @@
 use crate::catalog::table::Table;
+use crate::storage::row::Row;
+use crate::storage::table_store::{RowId, TableStore};
 use std::sync::Arc;
 
 pub(crate) struct TableEntry {
     table: Arc<Table>,
+    store: Arc<TableStore>,
 }
 
 impl TableEntry {
     pub(crate) fn new(table: Table) -> TableEntry {
         Self {
             table: Arc::new(table),
+            store: Arc::new(TableStore::new()),
         }
     }
 
-    pub fn table(&self) -> Arc<Table> {
+    pub(crate) fn table(&self) -> Arc<Table> {
         self.table.clone()
+    }
+
+    pub(crate) fn insert(&self, row: Row) -> RowId {
+        self.store.insert(row)
+    }
+
+    pub(crate) fn insert_all(&self, rows: Vec<Row>) {
+        self.store.insert_all(rows)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::schema::column::ColumnType;
+    use crate::schema::Schema;
+    use crate::storage::row::ColumnValue;
+
+    #[test]
+    fn insert_row() {
+        let table_entry = TableEntry::new(Table::new(
+            "employees".to_string(),
+            Schema::new().add_column("id", ColumnType::Int).unwrap(),
+        ));
+        table_entry.insert(Row::filled(vec![ColumnValue::Int(100)]));
+
+        let entries = table_entry.store.scan().collect::<Vec<_>>();
+        let rows = entries.iter().map(|entry| entry.value()).collect::<Vec<_>>();
+
+        assert_eq!(1, rows.len());
+        assert_eq!(100, rows[0].column_values()[0].int_value().unwrap());
+    }
+
+    #[test]
+    fn insert_rows() {
+        let table_entry = TableEntry::new(Table::new(
+            "employees".to_string(),
+            Schema::new().add_column("id", ColumnType::Int).unwrap(),
+        ));
+        table_entry.insert_all(
+            vec![
+                Row::filled(vec![ColumnValue::Int(10), ColumnValue::Text("relop".to_string())]),
+                Row::filled(vec![ColumnValue::Int(20), ColumnValue::Text("query".to_string())]),
+            ]
+        );
+
+        let entries = table_entry.store.scan().collect::<Vec<_>>();
+        let rows = entries.iter().map(|entry| entry.value()).collect::<Vec<_>>();
+
+        assert_eq!(2, rows.len());
+
+        assert!(rows.contains(&&Row::filled(vec![ColumnValue::Int(10), ColumnValue::Text("relop".to_string())])));
+        assert!(rows.contains(&&Row::filled(vec![ColumnValue::Int(20), ColumnValue::Text("query".to_string())])));
     }
 }
--- src/storage/row.rs
+++ src/storage/row.rs
@@ -9,6 +9,22 @@ pub enum ColumnValue {
     Text(String),
 }
 
+impl ColumnValue {
+    pub(crate) fn int_value(&self) -> Option<i64> {
+        if let ColumnValue::Int(value) = self {
+            return Some(*value);
+        }
+        None
+    }
+
+    pub(crate) fn text_value(&self) -> Option<&str> {
+        if let ColumnValue::Text(ref value) = self {
+            return Some(value);
+        }
+        None
+    }
+}
+
 impl Row {
     pub fn empty() -> Row {
         Self {values: Vec::new()}
@@ -67,5 +83,34 @@ mod tests {
         assert_eq!(2, row.column_value_count());
         assert_eq!(&ColumnValue::Text("relop".to_string()), row.column_value_at(0).unwrap());
         assert_eq!(&ColumnValue::Int(200), row.column_value_at(1).unwrap());
+    }
+}
+
+#[cfg(test)]
+mod column_value_tests {
+    use crate::storage::row::ColumnValue;
+
+    #[test]
+    fn int_value() {
+        let column_value = ColumnValue::Int(100);
+        assert_eq!(Some(100), column_value.int_value());
+    }
+
+    #[test]
+    fn attempt_to_get_int_value_for_a_non_int_column_type() {
+        let column_value = ColumnValue::Text("relop".to_string());
+        assert_eq!(None, column_value.int_value());
+    }
+
+    #[test]
+    fn text_value() {
+        let column_value = ColumnValue::Text("relop".to_string());
+        assert_eq!(Some("relop"), column_value.text_value());
+    }
+
+    #[test]
+    fn attempt_to_get_text_value_for_a_non_text_column_type() {
+        let column_value = ColumnValue::Int(100);
+        assert_eq!(None, column_value.text_value());
     }
 }
