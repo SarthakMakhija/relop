1a7288e6bc7d72ec62ea3efb4f6f3599c67b2b0f
Sarthak Makhija <sarthak.makhija@gmail.com>
2026-01-18 15:15:03 +0530

    Sarthak | Adds support for AND in lexer

--- src/query/lexer/mod.rs
+++ src/query/lexer/mod.rs
@@ -417,6 +417,28 @@ mod tests {
     }
 
     #[test]
+    fn lex_select_with_where_clause_with_and() {
+        assert_lex!(
+            "SELECT * FROM employees where name like 'rel%' and id = 1",
+            [
+                (TokenType::Keyword, "SELECT"),
+                (TokenType::Star, "*"),
+                (TokenType::Keyword, "FROM"),
+                (TokenType::Identifier, "employees"),
+                (TokenType::Keyword, "where"),
+                (TokenType::Identifier, "name"),
+                (TokenType::Keyword, "like"),
+                (TokenType::StringLiteral, "rel%"),
+                (TokenType::Keyword, "and"),
+                (TokenType::Identifier, "id"),
+                (TokenType::Equal, "="),
+                (TokenType::WholeNumber, "1"),
+                (TokenType::EndOfStream, ""),
+            ]
+        )
+    }
+
+    #[test]
     fn lex_select_with_where_clause_with_unsupported_operator() {
         let result =
             Lexer::new_with_default_keywords("select * from employees where id ! 10").lex();
