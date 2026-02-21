8f48cf88217b12246f1a1ea17622b40e2557ba36
Sarthak Makhija <sarthak.makhija@gmail.com>
2026-02-21 17:09:45 +0530

    Sarthak | Removes BatchError

--- src/storage/error.rs
+++ src/storage/error.rs
@@ -1,10 +1,3 @@
-/// Represents errors that can occur during batch operations.
-#[derive(Debug, PartialEq)]
-pub enum BatchError {
-    /// Indicates that a duplicate primary key was encountered in the batch.
-    DuplicatePrimaryKey,
-}
-
 /// Represents errors that can occur during comparison of row views.
 #[derive(Debug, PartialEq)]
 pub enum RowViewComparatorError {
